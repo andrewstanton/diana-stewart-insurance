058130227b0a7e6866f955351ba05ab7c26a1472
Andrew Stanton <andrew@toddprod.com>
2024-05-20 14:16:58 -0400

    Adding nav link

--- src/ui/molecules/Header.tsx
+++ src/ui/molecules/Header.tsx
@@ -6,6 +6,34 @@ import Button from "../atoms/Button"
 
 import logo from "../../../public/images/logo.jpg"
 
+export interface INavItem {
+  label: string
+  url: string
+}
+
+const nav: INavItem[] = [
+  {
+    label: "Home",
+    url: "/",
+  },
+  {
+    label: "About",
+    url: "/about",
+  },
+  {
+    label: "Health Insurance",
+    url: "/health-insurance",
+  },
+  {
+    label: "Testimonials",
+    url: "/testimonials",
+  },
+  {
+    label: "Contact",
+    url: "/contact",
+  },
+]
+
 const Header: FC = () => (
   <header className="w-full bg-white">
     <div className="grid grid-cols-2 justify-center items-center">
@@ -23,6 +51,21 @@ const Header: FC = () => (
         </Link>
       </div>
     </div>
+    <nav className="bg-green-500 text-white">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {nav.map((item) => (
+            <Link
+              href={item.url}
+              key={item.url}
+              className="px-4 py-4 font-bold text-center"
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </nav>
   </header>
 )
 
